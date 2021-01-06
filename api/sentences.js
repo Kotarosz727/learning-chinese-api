a4bcb0eccc3e66f594d763f32e335c037d128e8d
Kotarosz727 <yoga583sure246>
2021-01-06 11:02:37 +0900

    add post favorite

--- api/sentences.js
+++ api/sentences.js
@@ -1,24 +1,51 @@
-'use strict';
+"use strict";
 
-const {DynamoDB} = require("aws-sdk");
+const { DynamoDB } = require("aws-sdk");
 const db = new DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
-module.exports.fetchList = async() => {
+const dynamoDB = {
+  TableName: tableName,
+};
+
+module.exports.fetchList = async () => {
+  try {
+    const result = await db.scan(dynamoDB).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode,
+      body: error.message,
+    };
+  }
+};
+
+module.exports.postFavorite = async (event) => {
+  const data = JSON.parse(event.body);
   const params = {
-    TableName: tableName,
+    tableName: tableName,
+    Item: {
+      firstid: data.firstid,
+      secondid: data.userid,
+      chinese: data.chinese,
+      japanese: data.japanese,
+      pinin: data.pinin,
+    },
   };
 
   try {
-    const result = await db.scan(params).promise()
+    await db.put(params).promise();
     return {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(result),
     };
-  } catch(error) {
+  } catch (error) {
     return {
       statusCode: error.statusCode,
-      body: error.message
+      body: error.message,
     };
   }
 };
