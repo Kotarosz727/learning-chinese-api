6e70efc64fb2137237d83e62c8e2037d3ccfd0bb
Kotarosz727 <yoga583sure246>
2021-01-22 12:25:48 +0900

    add header

--- api/sentences.js
+++ api/sentences.js
@@ -13,6 +13,10 @@ module.exports.fetchList = async () => {
     const result = await db.scan(dynamoDB).promise();
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify(result),
     };
   } catch (error) {
